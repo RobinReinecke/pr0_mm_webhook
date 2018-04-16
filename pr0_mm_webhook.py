bb035def314f885c9a6edc80161b4f9f2cc8cb93
Robin Reinecke <robinreinecke@live.de>
2018-04-16 09:48:06 +0200

    improvements

--- pr0_mm_webhook.py
+++ pr0_mm_webhook.py
@@ -3,6 +3,7 @@ import requests
 import json
 import schedule
 import time
+import datetime
 
 class pr0_mm:
     """
@@ -38,12 +39,12 @@ class pr0_mm:
         Start the posting.
         """
 
-        print("Starting...")
+        print(self._getTimestamp() + "Starting...")
 
         # get latest post
         latest_post = self._API.get()[0]
 
-        print("Latest Post-ID: " + str(latest_post["id"]))
+        print(self._getTimestamp() + "Latest Post-ID: " + str(latest_post["id"]))
         # for correct order like on the website safe the "promoted" attribute
         # instead of the ID
         self._latest_post_promotion = latest_post["promoted"]
@@ -53,51 +54,70 @@ class pr0_mm:
             schedule.run_pending()
             time.sleep(1)
     
+
+    def _getTimestamp(self):
+        """
+        Return the current time as string.
+        
+        Returns:
+            string -- Current Time String
+        """
+
+        ts = time.time()
+        return datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S') + " "
+
     def _check(self):
         #payload for request
         payload = {"username": self.username, "icon_url": self.icon_url}
         #header for request
         headers = {'content-type': 'application/json'}
 
-        print("Checking for new posts...")
+        print(self._getTimestamp() + "Checking for new posts...")
         #fetch latest posts
         posts = self._API.get()
 
-        print("Latest Post-ID: " + str(posts[0]["id"]) + " and Promotion: " + str(posts[0]["promoted"]))
+        print(self._getTimestamp() + "Latest Post-ID: " + str(posts[0]["id"]) + " and Promotion: " + str(posts[0]["promoted"]))
         
-        #iterate over posts
-        for post in posts:
-            # if post isn't later then the known post, skip
-            if post["promoted"] <= self._latest_post_promotion:
-                continue
+        if self._latest_post_promotion == posts[0]["promoted"]:
+            print(self._getTimestamp + "No new Posts found.")
+        else:
+            #iterate over posts
+            for post in posts:
+                # if post isn't later then the newest known post, skip
+                if post["promoted"] <= self._latest_post_promotion:
+                    continue
 
-            #print the later post
-            print("Post found: " + str(post["id"]) + " and Promotion: " + str(post["promoted"]))
+                #print the later post
+                print(self._getTimestamp() + "Post found: " + str(post["id"]) + " and Promotion: " + str(post["promoted"]))
 
-            post_id = post["id"]
-            # get infos for this post
-            info = self._API.info(post_id)
+                post_id = post["id"]
+                # get infos for this post
+                info = self._API.info(post_id)
 
-            #send top 3 tags and link to the post to mattermost
-            text =  ("**" + info["tags"][0]["tag"] + "**   " +
-                        "**" + info["tags"][1]["tag"] + "**   " +
-                        "**" + info["tags"][2]["tag"] + "**   " +
-                        "\n**Link:** " + "http://pr0gramm.com//" + str(post["id"]) + "\n")
-            
+                #send top 3 tags and link to the post to mattermost
+                text =  ("**" + info["tags"][0]["tag"] + "**   " +
+                            "**" + info["tags"][1]["tag"] + "**   " +
+                            "**" + info["tags"][2]["tag"] + "**   " +
+                            "\n**Link:** " + "http://pr0gramm.com//" + str(post["id"]) + "\n")
+                
 
-            # add Text to payload
-            payload.update({"text": text })
-            # send request to webhook
-            requests.post(self.url, data=json.dumps(payload), headers=headers)
+                # add Text to payload
+                payload.update({"text": text })
+                # send request to webhook
+                requests.post(self.url, data=json.dumps(payload), headers=headers)
 
 
-            # if the post is an image or gif, post the direct link as additional message 
-            # to the webhook. Mattermost only embedds images when they stand alone in a 
-            # message.
-            if post["image"].endswith((".jpg",".png",".gif")):
-                payload["text"] = "http://img.pr0gramm.com/" + post["image"]
+                # if the post is an image or gif, post the direct link as additional message 
+                # to the webhook. Mattermost only embedds images when they stand alone in a 
+                # message.
+                if post["image"].endswith((".jpg",".png",".gif")):
+                    payload["text"] = "http://img.pr0gramm.com/" + post["image"]
+                # else post the thumbnail
+                else:
+                    payload["text"] = "http://img.pr0gramm.com/" + post["thumb"]
+
                 r = requests.post(self.url, data=json.dumps(payload), headers=headers)
 
+
+        # remember the newest post
         self._latest_post_promotion = posts[0]["promoted"]
-
-
